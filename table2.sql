8719c0d3afb69fedc0e7dbcc4f7d76923bc4653d
эля <ela@MacBook-Air-ela.local>
2025-02-17 14:46:32 +0300

    few

--- table2.sql
+++ table2.sql
@@ -1,18 +1,29 @@
-CREATE TABLE boo(
+CREATE TABLE bookrf(
     book_id SERIAL PRIMARY KEY,
     title VARCHAR(50),
     author VARCHAR(30),
     price DECIMAL(8,2),
     amount INT
 );
-INSERT INTO boo (title, author, price, amount)
+INSERT INTO bookrf (title, author, price, amount)
 VALUES 
     ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
     ('Белая гвардия', 'Булгаков М.А.', 540.50, 5),
     ('Идиот', 'Достоевский Ф.М.', 460.00, 10),
-    ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
+    ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2),
+	('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15),
+	('Лирика', 'Гумилев Н.С.', 460.00, 10),
+	('Поэмы', 'Бехтерев С.С.', 460.00, 10),
+	('Капитанская дочка', 'Пушкин А.С.', 520.50, 7),
+	('Дети полуночи', 'Рушди Салман', 950.00, 5);
 
-SELECT author, title, price, amount
+SELECT author, title, price, amount 
 FROM boo
-WHERE amount BETWEEN 2 AND 14
+WHERE 
+    title LIKE '% %'
+    AND (
+		amount BETWEEN 2 AND 14 
+    	AND author LIKE '% С.%'
+        OR author LIKE '%С.%'
+    )
 ORDER BY author DESC, title;
